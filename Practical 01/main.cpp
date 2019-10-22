092e1bc716f35f5e3015442d32df9ff4061eb7e7
Tang Hong Jie <30696041+jtmycraft@users.noreply.github.com>
2019-10-22 21:15:59 +0800

    Add files via upload

---
+++ Practical 01/main.cpp
@@ -0,0 +1,160 @@
+#include <Windows.h>
+#include <gl/GL.h>
+
+//#pragma comment (lib, "OpenGL32.lib")
+
+#define WINDOW_TITLE "OpenGL Window"
+
+
+LRESULT WINAPI WindowProcedure(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch (msg)
+	{
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+
+	case WM_KEYDOWN:
+		if (wParam == VK_ESCAPE) PostQuitMessage(0);
+		break;
+
+	default:
+		break;
+	}
+
+	return DefWindowProc(hWnd, msg, wParam, lParam);
+}
+//--------------------------------------------------------------------
+
+bool initPixelFormat(HDC hdc)
+{
+	PIXELFORMATDESCRIPTOR pfd;
+	ZeroMemory(&pfd, sizeof(PIXELFORMATDESCRIPTOR));
+
+	pfd.cAlphaBits = 8;
+	pfd.cColorBits = 32;
+	pfd.cDepthBits = 24;
+	pfd.cStencilBits = 0;
+
+	pfd.dwFlags = PFD_DOUBLEBUFFER | PFD_SUPPORT_OPENGL | PFD_DRAW_TO_WINDOW;
+
+	pfd.iLayerType = PFD_MAIN_PLANE;
+	pfd.iPixelType = PFD_TYPE_RGBA;
+	pfd.nSize = sizeof(PIXELFORMATDESCRIPTOR);
+	pfd.nVersion = 1;
+
+	// choose pixel format returns the number most similar pixel format available
+	int n = ChoosePixelFormat(hdc, &pfd);
+
+	// set pixel format returns whether it sucessfully set the pixel format
+	if (SetPixelFormat(hdc, n, &pfd))
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+//--------------------------------------------------------------------
+
+void display()
+{
+	//--------------------------------
+	//	OpenGL drawing
+	//--------------------------------
+
+	glClearColor(0.8f, 0.8f, 0.8f, 0.0f); // <--- Set Color
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	///* Demo 1: Solid Triangle
+	glBegin(GL_TRIANGLES);
+	//*/
+
+	/* Demo 2: Wireframe Triangles and Width
+	glLineWidth(3); // <--- Set Line Width
+	glBegin(GL_LINE_LOOP);
+	*/
+
+	/* Demo 3: Dot Triangles and Dot Size
+	glPointSize(3); // <--- Set Dot Size
+	glBegin(GL_POINTS);
+	*/
+
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glVertex2f(0, 0.5);
+	glColor3f(0.0f, 1.0f, 0.0f);
+	glVertex2f(-0.5, 0);
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex2f(0.5, 0);
+
+	glEnd();
+
+	//--------------------------------
+	//	End of OpenGL drawing
+	//--------------------------------
+}
+//--------------------------------------------------------------------
+
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE prevInstance,
+	_In_ LPSTR cmdLine, _In_ int nCmdShow) // FIXED
+{
+	WNDCLASSEX wc;
+	ZeroMemory(&wc, sizeof(WNDCLASSEX));
+
+	wc.cbSize = sizeof(WNDCLASSEX);
+	wc.hInstance = GetModuleHandle(NULL);
+	wc.lpfnWndProc = WindowProcedure;
+	wc.lpszClassName = WINDOW_TITLE;
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+
+	if (!RegisterClassEx(&wc)) return false;
+
+	HWND hWnd = CreateWindow(WINDOW_TITLE, WINDOW_TITLE, WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT, 800, 600,
+		NULL, NULL, wc.hInstance, NULL);
+
+	//--------------------------------
+	//	Initialize window for OpenGL
+	//--------------------------------
+
+	HDC hdc = GetDC(hWnd);
+
+	//	initialize pixel format for the window
+	initPixelFormat(hdc);
+
+	//	get an openGL context
+	HGLRC hglrc = wglCreateContext(hdc);
+
+	//	make context current
+	if (!wglMakeCurrent(hdc, hglrc)) return false;
+
+	//--------------------------------
+	//	End initialization
+	//--------------------------------
+
+	ShowWindow(hWnd, nCmdShow);
+
+	MSG msg;
+	ZeroMemory(&msg, sizeof(msg));
+
+	while (true)
+	{
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			if (msg.message == WM_QUIT) break;
+
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+
+		display();
+
+		SwapBuffers(hdc);
+	}
+
+	UnregisterClass(WINDOW_TITLE, wc.hInstance);
+
+	return true;
+}
+//--------------------------------------------------------------------
---
+++ Practical 01/p1q1.cpp
@@ -0,0 +1,379 @@
+#include <Windows.h>
+#include <gl/GL.h>
+#include <gl/GLU.h>
+#include <iostream>
+#include <conio.h>
+#include <string>
+
+//#pragma comment (lib, "OpenGL32.lib")
+
+#define WINDOW_TITLE "OpenGL Window"
+
+float r = 1, g = 0, b = 0;
+float points[4][2] = {
+	{ -0.1f, 0.05f },{ 0.1f, 0.05f },{ 0.1f, -0.05f },{ -0.1f, -0.05f }
+};
+
+int num = 0;
+
+LRESULT WINAPI WindowProcedure(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch (msg)
+	{
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+
+	case WM_CHAR:
+		break;
+
+	case WM_KEYDOWN:
+		switch (wParam)
+		{
+		case VK_ESCAPE:
+			PostQuitMessage(0);
+			break;
+		case VK_LEFT:
+			num--;
+			break;
+		case VK_RIGHT:
+			num++;
+			break;
+		}
+		break;
+
+	default:
+		break;
+	}
+
+	return DefWindowProc(hWnd, msg, wParam, lParam);
+}
+//--------------------------------------------------------------------
+
+bool initPixelFormat(HDC hdc)
+{
+	PIXELFORMATDESCRIPTOR pfd;
+	ZeroMemory(&pfd, sizeof(PIXELFORMATDESCRIPTOR));
+
+	pfd.cAlphaBits = 8;
+	pfd.cColorBits = 32;
+	pfd.cDepthBits = 24;
+	pfd.cStencilBits = 0;
+
+	pfd.dwFlags = PFD_DOUBLEBUFFER | PFD_SUPPORT_OPENGL | PFD_DRAW_TO_WINDOW;
+
+	pfd.iLayerType = PFD_MAIN_PLANE;
+	pfd.iPixelType = PFD_TYPE_RGBA;
+	pfd.nSize = sizeof(PIXELFORMATDESCRIPTOR);
+	pfd.nVersion = 1;
+
+	// choose pixel format returns the number most similar pixel format available
+	int n = ChoosePixelFormat(hdc, &pfd);
+
+	// set pixel format returns whether it sucessfully set the pixel format
+	if (SetPixelFormat(hdc, n, &pfd))
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+//--------------------------------------------------------------------
+
+void DrawCircle(float cx, float cy, float r, int num_segments) {
+	glBegin(GL_POLYGON);
+	for (int ii = 0; ii < num_segments; ii++) {
+		float theta = 2.0f * 3.1415926f * float(ii) / float(num_segments);//get the current angle 
+		float x = r * cosf(theta);//calculate the x component 
+		float y = r * sinf(theta);//calculate the y component 
+		glVertex2f(x + cx, y + cy);//output vertex 
+	}
+	glEnd();
+}
+
+void drawCircle()
+{
+	float x1 = 0, y1 = 0, x2 = 0, y2 = 0;
+	float angle = 0, radius = 0.5;
+	glBegin(GL_TRIANGLE_FAN);
+	glVertex2f(x1, y1);
+
+	for (angle = 0; angle <= 360.0; angle += 0.2) {
+		x2 = x1 + cos(angle)*radius;
+		y2 = y1 + sin(angle)*radius;
+		glVertex2f(x2, y2);
+	}
+
+	glEnd();
+}
+
+void drawFlag(int num)
+{
+	switch (num)
+	{
+	case -1:
+		num = 5;
+
+		break;
+	case 0:
+		// Pahang
+		glBegin(GL_QUADS);
+		{
+			glColor3ub(255, 255, 255);
+
+			glVertex2f(-0.9f, 0.9f);
+			glVertex2f(0.9f, 0.9f);
+			glVertex2f(0.9f, 0);
+			glVertex2f(-0.9f, 0);
+		}
+		glEnd();
+
+		glBegin(GL_QUADS);
+		{
+			glColor3ub(0, 0, 0);
+
+			glVertex2f(-0.9, 0);
+			glVertex2f(0.9f, 0);
+			glVertex2f(0.9f, -0.9f);
+			glVertex2f(-0.9f, -0.9f);
+		}
+		glEnd();
+
+		break;
+	case 1:
+		// Negeri Sembilan
+		glBegin(GL_QUADS);
+		{
+			glColor3ub(255, 255, 0);
+
+			glVertex2f(-0.9f, 0.9f);
+			glVertex2f(0.9f, 0.9f);
+			glVertex2f(0.9f, -0.9f);
+			glVertex2f(-0.9f, -0.9f);
+		}
+		glEnd();
+
+		glBegin(GL_TRIANGLES);
+		{
+			glColor3ub(255, 0, 0);
+
+			glVertex2f(-0.9f, 0.9f);
+			glVertex2f(0, 0.9f);
+			glVertex2f(0, 0);
+		}
+		glEnd();
+
+		glBegin(GL_TRIANGLES);
+		{
+			glColor3ub(59, 0, 0);
+
+			glVertex2f(-0.9f, 0.9f);
+			glVertex2f(-0.9f, 0);
+			glVertex2f(0, 0);
+		}
+		glEnd();
+
+		break;
+	case 2:
+		glBegin(GL_QUADS);
+		{
+			glColor3ub(255, 255, 255);
+
+			glVertex2f(-0.9f, 0.9f);
+			glVertex2f(0.9f, 0.9f);
+			glVertex2f(0.9f, -0.9f);
+			glVertex2f(-0.9f, -0.9f);
+		}
+		glEnd();
+
+		glBegin(GL_QUADS);
+		{
+			glColor3ub(255, 0, 0);
+
+			glVertex2f(-0.15f, 0.9f);
+			glVertex2f(0.15f, 0.9f);
+			glVertex2f(0.15f, -0.9f);
+			glVertex2f(-0.15f, -0.9f);
+		}
+		glEnd();
+
+		glBegin(GL_QUADS);
+		{
+			glColor3ub(255, 0, 0);
+
+			glVertex2f(-0.9f, 0.15f);
+			glVertex2f(0.9f, 0.15f);
+			glVertex2f(0.9f, -0.15f);
+			glVertex2f(-0.9f, -0.15f);
+		}
+		glEnd();
+
+		break;
+	case 3:
+		glBegin(GL_QUADS);
+		{
+			glColor3ub(0, 183, 255);
+
+			glVertex2f(-0.9f, 0.9f);
+			glVertex2f(0.9f, 0.9f);
+			glVertex2f(0.9f, -0.9f);
+			glVertex2f(-0.9f, -0.9f);
+		}
+		glEnd();
+
+		glBegin(GL_POLYGON);
+		{
+			glColor3ub(255, 255, 255);
+
+			glVertex2f(-0.9f, 0.9f);
+			glVertex2f(-0.7f, 0.9f);
+			glVertex2f(0.9f, -0.7f);
+			glVertex2f(0.9, -0.9f);
+			glVertex2f(0.7f, -0.9f);
+			glVertex2f(-0.9f, 0.7f);
+		}
+		glEnd();
+
+		glRotatef(90, 0, 0, 1);
+		glBegin(GL_POLYGON);
+		{
+			glColor3ub(255, 255, 255);
+
+			glVertex2f(-0.9f, 0.9f);
+			glVertex2f(-0.7f, 0.9f);
+			glVertex2f(0.9f, -0.7f);
+			glVertex2f(0.9, -0.9f);
+			glVertex2f(0.7f, -0.9f);
+			glVertex2f(-0.9f, 0.7f);
+		}
+		glEnd();
+
+		break;
+	case 4:
+		glBegin(GL_QUADS);
+		{
+			glColor3ub(255, 255, 255);
+
+			glVertex2f(-0.9f, 0.9f);
+			glVertex2f(0.9f, 0.9f);
+			glVertex2f(0.9f, -0.9f);
+			glVertex2f(-0.9f, -0.9f);
+		}
+		glEnd();
+
+		glColor3ub(255, 0, 0);
+		drawCircle();
+
+		break;
+	case 5:
+		glPushMatrix();
+		glColor3ub(255, 255, 0);
+		DrawCircle(0, 0, 0.35f, 100);
+		glPopMatrix();
+
+		glPushMatrix();
+		glColor3ub(0, 0, 0);
+		glTranslatef(-0.15f, 0.1f, 0);
+		DrawCircle(0, 0, 0.01f, 100);
+		glPopMatrix();
+
+		glPushMatrix();
+		glColor3ub(0, 0, 0);
+		glTranslatef(0.15f, 0.1f, 0);
+		DrawCircle(0, 0, 0.01f, 100);
+		glPopMatrix();
+
+		glPushMatrix();
+		glRotatef(180, 1, 0, 0);
+		glBegin(GL_LINE_STRIP);
+		for (int ii = 0; ii < 100; ii++) {
+			float theta = 3.1415926f * float(ii) / float(100);//get the current angle 
+			float x = 0.3f * cosf(theta);//calculate the x component 
+			float y = 0.3f * sinf(theta);//calculate the y component 
+			glVertex2f(x + 0, y + 0);//output vertex 
+		}
+		glEnd();
+		glPopMatrix();
+
+		break;
+	case 6:
+		num = 0;
+
+		break;
+	}
+}
+
+void display()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	glClearColor(0.55f, 0.55f, 0.55f, 0);
+
+	drawFlag(num);
+}
+
+//--------------------------------------------------------------------
+
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE prevInstance,
+	_In_ LPSTR cmdLine, _In_ int nCmdShow) // FIXED
+{
+	WNDCLASSEX wc;
+	ZeroMemory(&wc, sizeof(WNDCLASSEX));
+
+	wc.cbSize = sizeof(WNDCLASSEX);
+	wc.hInstance = GetModuleHandle(NULL);
+	wc.lpfnWndProc = WindowProcedure;
+	wc.lpszClassName = WINDOW_TITLE;
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+
+	if (!RegisterClassEx(&wc)) return false;
+
+	HWND hWnd = CreateWindow(WINDOW_TITLE, WINDOW_TITLE, WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT, 800, 600,
+		NULL, NULL, wc.hInstance, NULL);
+
+	//--------------------------------
+	//	Initialize window for OpenGL
+	//--------------------------------
+
+	HDC hdc = GetDC(hWnd);
+
+	//	initialize pixel format for the window
+	initPixelFormat(hdc);
+
+	//	get an openGL context
+	HGLRC hglrc = wglCreateContext(hdc);
+
+	//	make context current
+	if (!wglMakeCurrent(hdc, hglrc)) return false;
+
+	//--------------------------------
+	//	End initialization
+	//--------------------------------
+
+	ShowWindow(hWnd, nCmdShow);
+
+	MSG msg;
+	ZeroMemory(&msg, sizeof(msg));
+
+	while (true)
+	{
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			if (msg.message == WM_QUIT) break;
+
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+
+		display();
+
+		SwapBuffers(hdc);
+	}
+
+	UnregisterClass(WINDOW_TITLE, wc.hInstance);
+
+	return true;
+}
+//--------------------------------------------------------------------
